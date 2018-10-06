df3ede5939fbb51f826761d6a9a0925faafe2707
Aleksey Chervyakov <alex.iver23@gmail.com>
2018-10-06 15:20:06 +0700

    Правка стиля для сплеш активити

--- app/src/main/java/design/alex/starwars/SplashScreenActivity.java
+++ app/src/main/java/design/alex/starwars/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_activity);
-        new Handler().postDelayed(new Runnable(){
+        /*new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 Intent intent = new Intent(
@@ -21,6 +22,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3200);
+        }, 3200);*/
     }
 }
